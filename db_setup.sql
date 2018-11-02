b39092816ad1d3bdba4516c7cf912379d09982eb
Josh Stoltenberg <jhstoltenberg@gmail.com>
2018-11-02 16:28:39 -0500

    here's the version that actually created a db sucessfully

--- db_setup.sql
+++ db_setup.sql
@@ -10,27 +10,27 @@ SET SCHEMA 'ers';
 CREATE TABLE ers_reimbursment
 (
     reimb_id SERIAL,
-    reimb_amt NUMBER NOT NULL,
+    reimb_amt NUMERIC(5,2) NOT NULL,
     reimb_submitted TIMESTAMP NOT NULL,
     reimb_resolved TIMESTAMP,
-    reimb_description VARCHAR2(250),
-    reimb_receipt BLOB,
-    reimb_author NUMBER NOT NULL,
-    reimb_resolver NUMBER,
-    reimb_status_id NUMBER NOT NULL,
-    reimb_type_id NUMBER NOT NULL,
+    reimb_description VARCHAR(250),
+    --reimb_receipt BLOB,
+    reimb_author INTEGER NOT NULL,
+    reimb_resolver INTEGER,
+    reimb_status_id INTEGER NOT NULL,
+    reimb_type_id INTEGER NOT NULL,
     CONSTRAINT ers_reimbursment_PK PRIMARY KEY (reimb_id)
 );
 
 CREATE TABLE ers_users
 (
     ers_users_id SERIAL,
-    ers_username VARCHAR2(50) NOT NULL,
-    ers_password VARCHAR2(50) NOT NULL,
-    user_first_name VARCHAR2(100) NOT NULL,
-    user_last_name  VARCHAR2(100) NOT NULL,
-    user_email  VARCHAR2(150) NOT NULL,
-    user_role_id NUMBER NOT NULL,
+    ers_username VARCHAR(50) NOT NULL,
+    ers_password VARCHAR(50) NOT NULL,
+    user_first_name VARCHAR(100) NOT NULL,
+    user_last_name  VARCHAR(100) NOT NULL,
+    user_email  VARCHAR(150) NOT NULL,
+    user_role_id INTEGER NOT NULL,
     CONSTRAINT ers_users_PK PRIMARY KEY (ers_users_id),
     CONSTRAINT ers_users__UNv1 CHECK (ers_username <> user_email)
 );
@@ -38,36 +38,39 @@ CREATE TABLE ers_users
 CREATE TABLE ers_reimbursment_status
 (
     reimb_status_id SERIAL,
-    reimb_status  VARCHAR2(10) NOT NULL,
+    reimb_status  VARCHAR(10) NOT NULL,
     CONSTRAINT reimb_status_PK PRIMARY KEY (reimb_status_id)
 );
 
 CREATE TABLE ers_reimbursment_type
 (
     reimb_type_id SERIAL,
-    reimb_type VARCHAR2(10) NOT NULL,
+    reimb_type VARCHAR(10) NOT NULL,
     CONSTRAINT reimb_type_PK PRIMARY KEY (reimb_type_id)
 );
 
 CREATE TABLE ers_user_roles
 (
     ers_user_role_id SERIAL,
-    user_role VARCHAR2(10) NOT NULL
+    user_role VARCHAR(10) NOT NULL,
     CONSTRAINT ers_user_roles_PK PRIMARY KEY (ers_user_role_id)
 );
 
--- Add foreign keys
+--add foreign keys
 ALTER TABLE ers_reimbursment 
 ADD CONSTRAINT ers_users_FK_auth FOREIGN KEY (reimb_author) REFERENCES ers_users (ers_users_id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-    CONSTRAINT ers_users_FK_reslvr FOREIGN KEY (reimb_resolver) REFERENCES ers_users (ers_users_id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION,
+ALTER TABLE ers_reimbursment
+ADD CONSTRAINT ers_users_FK_reslvr FOREIGN KEY (reimb_resolver) REFERENCES ers_users (ers_users_id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-    CONSTRAINT ers_reimbursment_status_FK FOREIGN KEY (reimb_status_id) REFERENCES ers_reimbursment_status (reimb_status_id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION,
+ALTER TABLE ers_reimbursment
+ADD CONSTRAINT ers_reimbursment_status_FK FOREIGN KEY (reimb_status_id) REFERENCES ers_reimbursment_status (reimb_status_id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-    CONSTRAINT ers_reimbursment_type_FK FOREIGN KEY (reimb_type_id) REFERENCES ers_reimbursment_type (reimb_status_id)
+ALTER TABLE ers_reimbursment
+ADD CONSTRAINT ers_reimbursment_type_FK FOREIGN KEY (reimb_type_id) REFERENCES ers_reimbursment_type (reimb_type_id)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE ers_users 
