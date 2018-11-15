912949a874bee91a2f0d32bf09e34da87befde5d
Josh S <jhstoltenberg@gmail.com>
2018-11-15 15:41:43 -0600

    change to allow EB propety use

--- ERS/src/main/java/com/revature/util/ConnectionUtil.java
+++ ERS/src/main/java/com/revature/util/ConnectionUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-	
+
 	static {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -14,14 +14,30 @@ public class ConnectionUtil {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Connection getConnection() throws SQLException {
-		String url = System.getenv("db_url");
-		String port = System.getenv("db_port");
-		String dbName = System.getenv("db_name");
-		String dbSchema = System.getenv("db_schema");
-		String username = System.getenv("db_username");
-		String password = System.getenv("db_password");
+		String url;
+		String port;
+		String dbName;
+		String dbSchema;
+		String username;
+		String password;
+		if ("USE_PROPERTY".equals(System.getProperty("USE_PROPERTY"))) {
+			url = System.getProperty("db_url");
+			port = System.getProperty("db_port");
+			dbName = System.getProperty("db_name");
+			dbSchema = System.getProperty("db_schema");
+			username = System.getProperty("db_username");
+			password = System.getProperty("db_password");
+
+		} else {
+			url = System.getenv("db_url");
+			port = System.getenv("db_port");
+			dbName = System.getenv("db_name");
+			dbSchema = System.getenv("db_schema");
+			username = System.getenv("db_username");
+			password = System.getenv("db_password");
+		}
 
 		String dataSource = "jdbc:postgresql://" + url + ":" + port + "/" + dbName + "?currentSchema=" + dbSchema;
 
